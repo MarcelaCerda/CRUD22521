efd93baf5d192ac49fb98997e44725851be23a5c
marcelabueeduar <marcela.cerda@bue.edu.ar>
2022-12-01 13:46:07 -0300

    1/12/2022

---
+++ src/app.py
@@ -0,0 +1,51 @@
+from flask import Flask ,jsonify ,request
+# del modulo flask importar la clase Flask y los métodos jsonify,request
+from flask_cors import CORS       # del modulo flask_cors importar CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+app=Flask(__name__)  # crear el objeto app de la clase Flask
+CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
+# configuro la base de datos, con el nombre el usuario y la clave
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
+# desde el objeto app configuro la URI de la BBDD    user:clave@localhost/nombreBBDD
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db= SQLAlchemy(app)
+ma=Marshmallow(app)
+ 
+class Producto(db.Model):   # la clase Producto hereda de db.Model    
+    id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
+    nombre=db.Column(db.String(100))
+    precio=db.Column(db.Integer)
+    stock=db.Column(db.Integer)
+    imagen=db.Column(db.String(400))
+    def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
+        self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+        self.precio=precio
+        self.stock=stock
+        self.imagen=imagen
+with app.app_context():
+    db.create_all()  # crea las tablas
+#  ************************************************************
+ 
+class ProductoSchema(ma.Schema):
+    class Meta:
+        fields=('id','nombre','precio','stock',"imagen")
+
+producto_schema=ProductoSchema()            # para crear un producto
+productos_schema=ProductoSchema(many=True)  # multiples registros
+ 
+# crea los endpoint o rutas (json)
+@app.route('/productos',methods=['GET'])
+def get_Productos():
+    all_productos=Producto.query.all()     # query.all() lo hereda de db.Model
+    result=productos_schema.dump(all_productos)  # .dump() lo hereda de ma.schema
+    return jsonify(result)
+ 
+@app.route('/productos/<id>',methods=['GET'])
+def get_producto(id):
+    producto=Producto.query.get(id)
+    return producto_schema.jsonify(producto)
+ 
+# programa principal *******************************
+if __name__=='__main__':  
+    app.run(debug=True, port=5000)  
