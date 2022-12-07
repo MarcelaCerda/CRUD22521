3123c9601724f68e82672ae93e32d25692f96b72
marcelabueeduar <marcela.cerda@bue.edu.ar>
2022-12-06 22:38:37 -0300

    20221206

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask_marshmallow import Marshmallow
 app=Flask(__name__)  # crear el objeto app de la clase Flask
 CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto22521'
 # desde el objeto app configuro la URI de la BBDD    user:clave@localhost/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db= SQLAlchemy(app)
@@ -29,7 +29,7 @@ with app.app_context():
  
 class ProductoSchema(ma.Schema):
     class Meta:
-        fields=('id','nombre','precio','stock',"imagen")
+        fields=('id','nombre','precio','stock',"imagen")#
 
 producto_schema=ProductoSchema()            # para crear un producto
 productos_schema=ProductoSchema(many=True)  # multiples registros
@@ -45,7 +45,43 @@ def get_Productos():
 def get_producto(id):
     producto=Producto.query.get(id)
     return producto_schema.jsonify(producto)
+
+@app.route('/productos/<id>',methods=['DELETE'])
+def delete_producto(id):
+    producto=Producto.query.get(id)
+    db.session.delete(producto)
+    db.session.commit()
+    return producto_schema.jsonify(producto)
+
+@app.route('/productos', methods=['POST']) # crea ruta o endpoint
+def create_producto():
+    print(request.json)  # request.json contiene el json que envio el cliente
+    nombre=request.json['nombre']
+    precio=request.json['precio']
+    stock=request.json['stock']
+    imagen=request.json['imagen']
+    new_producto=Producto(nombre,precio,stock,imagen)
+    db.session.add(new_producto)
+    db.session.commit()
+    return producto_schema.jsonify(new_producto)
+
+@app.route('/productos/<id>' ,methods=['PUT'])
+def update_producto(id):
+    producto=Producto.query.get(id)
+   
+    nombre=request.json['nombre']
+    precio=request.json['precio']
+    stock=request.json['stock']
+    imagen=request.json['imagen']
  
+    producto.nombre=nombre
+    producto.precio=precio
+    producto.stock=stock
+    producto.imagen=imagen
+    db.session.commit()
+    return producto_schema.jsonify(producto)
+
+
 # programa principal *******************************
 if __name__=='__main__':  
     app.run(debug=True, port=5000)  
